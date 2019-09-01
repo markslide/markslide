6d96ebabc53d144fd341f228545afbae0f1b9175
John_Theo <B_W_Ghost@163.com>
2019-09-01 23:52:22 +0800

    change layout

--- src/pages/upload.page.tsx
+++ src/pages/upload.page.tsx
@@ -4,15 +4,29 @@ import {Upload} from '@/components/upload'
 import {RouteComponentProps} from 'react-router'
 import {useStore} from 'reto'
 import {PresentationStore} from '@/stores/presentation.store'
+import {createGlobalStyle} from "styled-components";
+import {Preview} from "@/components/preview";
+
+
+const HrStyle = createGlobalStyle`
+  hr {
+    border: 0.5px dashed #bbb;
+    margin-block-start: 2em;
+    margin-block-end: 2em;
+  }
+`
 
 export const UploadPage: FC<RouteComponentProps> = (props) => {
   const store = useStore(PresentationStore)
   
   return (
-    <div>
+    <div style={{height: '100vh', display: 'flex', flexDirection: 'row'}}>
       <Upload onUpload={(text) => {
         store.setText(text)
       }}/>
+      <HrStyle/>
+      <hr/>
+      <Preview/>
       <button onClick={()=>props.history.push('/presentation')}>Show</button>
     </div>
   )
