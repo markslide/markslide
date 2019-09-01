2493723966879ed08fa40ae356b958784a6df6d6
John_Theo <B_W_Ghost@163.com>
2019-09-01 23:48:31 +0800

    add preview comp

---
+++ src/components/preview.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react'
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export const Preview: FC = () => {
+
+  return (
+    <Container id='preview'>
+    </Container>
+  )
+}
