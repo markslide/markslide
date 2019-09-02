6c7890be5012e361832cba34f2efff460ab71953
卡晨 <guangbo.hgb@antfin.com>
2019-09-02 13:28:01 +0800

    slide component

---
+++ src/components/slide.tsx
@@ -0,0 +1,115 @@
+import styled, {keyframes} from 'styled-components'
+import React, {memo, useMemo} from 'react'
+import {markdownToHtml} from '@/utils/markdown-to-html'
+
+const moveFromRightKeyframes = keyframes`
+  to { transform: translateX(-100%); }
+`
+
+const moveFromLeftKeyframes = keyframes`
+  to { transform: translateX(100%); }
+`
+
+const rotateRightSideFirstKeyframes = keyframes`
+  0% { }
+  40% { transform: rotateY(15deg); opacity: .8; animation-timing-function: ease-out; }
+  100% { transform: scale(0.8) translateZ(-200px); opacity:0; }
+`
+
+const rotateLeftSideFirstKeyframes = keyframes`
+  0% { }
+  40% { transform: rotateY(-15deg); opacity: .8; animation-timing-function: ease-out; }
+  100% { transform: scale(0.8) translateZ(-200px); opacity:0; }
+`
+
+const Markdown = styled.div.attrs((props) => ({
+  className: `markdown ${props.className}`,
+}))`
+  width:80vw;
+  height: 80vh;
+  overflow-y: scroll;
+  position: absolute;
+  top:0;
+  padding:10vh 10vw;
+  user-select: none;
+  background: #fff;
+  &.previous {
+    z-index: 6;
+    left:-100vw;
+    &.transit-previous {
+      animation: ${moveFromLeftKeyframes} .6s ease both;
+      animation-delay: .2s;
+    }
+  }
+  &.current {
+    z-index: 5;
+    left:0;
+    &.transit-previous {
+      transform-origin: 0 50%;
+      animation: ${rotateLeftSideFirstKeyframes} .8s both ease-in;
+    }
+    &.transit-next {
+      transform-origin: 0 50%;
+      animation: ${rotateRightSideFirstKeyframes} .8s both ease-in;
+    }
+  }
+  &.next {
+    z-index: 7;
+    left: 100vw;
+    &.transit-next {
+      animation: ${moveFromRightKeyframes} .6s ease both;
+      animation-delay: .2s;
+    }
+  }
+`
+
+const Content = styled.div.attrs(() => ({
+  className: 'content'
+}))`
+  @media all{
+    font-size: 12px;
+  }
+  @media all and (min-width: 200px) {
+    font-size: 16px;
+  }
+  @media all and (min-width: 400px) {
+    font-size: 20px;
+  }
+  @media all and (min-width: 600px) {
+    font-size: 24px;
+  }
+  @media all and (min-width: 800px) {
+    font-size: 28px;
+  }
+  @media all and (min-width: 1000px) {
+    font-size: 32px;
+  }
+  @media all and (min-width: 1200px){
+    font-size: 36px;
+  }
+  
+  table {
+    width: 100%;
+  }
+`
+
+export enum SlideMode {
+  previous = 'previous',
+  current = 'current',
+  next = 'next',
+}
+
+interface Props {
+  transit?: 'previous' | 'next'
+  mode?: SlideMode
+  markdown: string
+}
+
+export const Slide = memo<Props>((props) => {
+  const html = useMemo(() => markdownToHtml(props.markdown), [props.markdown])
+  return (
+    <Markdown className={(props.mode || '') + ' ' + `transit-${props.transit}`}>
+      <Content dangerouslySetInnerHTML={{__html: html}}></Content>
+    </Markdown>
+  )
+})
--- src/pages/presentation.page.tsx
+++ src/pages/presentation.page.tsx
@@ -1,32 +1,12 @@
-import {FC, useEffect, useLayoutEffect, useMemo, useRef, useState} from 'react'
+import {FC, useEffect, useLayoutEffect, useRef, useState} from 'react'
 import {RouteComponentProps} from 'react-router'
 import * as React from 'react'
-import styled, {css, keyframes} from 'styled-components'
+import styled from 'styled-components'
 import {useStore} from 'reto'
 import {PresentationStore} from '@/stores/presentation.store'
-import {markdownToHtml} from '@/utils/markdown-to-html'
 import * as mousetrap from 'mousetrap'
 import '@/themes/github.less'
-
-const moveFromRightKeyframes = keyframes`
-  to { transform: translateX(-100%); }
-`
-
-const moveFromLeftKeyframes = keyframes`
-  to { transform: translateX(100%); }
-`
-
-const rotateRightSideFirstKeyframes = keyframes`
-  0% { }
-  40% { transform: rotateY(15deg); opacity: .8; animation-timing-function: ease-out; }
-  100% { transform: scale(0.8) translateZ(-200px); opacity:0; }
-`
-
-const rotateLeftSideFirstKeyframes = keyframes`
-  0% { }
-  40% { transform: rotateY(-15deg); opacity: .8; animation-timing-function: ease-out; }
-  100% { transform: scale(0.8) translateZ(-200px); opacity:0; }
-`
+import {Slide, SlideMode} from '@/components/slide'
 
 
 const Container = styled.div`
@@ -56,89 +36,6 @@ const Background = styled.div<{
   background: #000000;
 `
 
-const Markdown = styled.div`
-  width:80vw;
-  height: 80vh;
-  overflow-y: scroll;
-  position: absolute;
-  top:0;
-  padding:10vh 10vw;
-  user-select: none;
-  background: #fff;
-`
-
-const previousMixin = css`
-  animation: ${moveFromLeftKeyframes} .6s ease both;
-  animation-delay: .2s;
-`
-const PreviousMarkdown = styled(Markdown)<{
-  transitRight: boolean
-}>`
-  z-index: 6;
-  left:-100vw;
-  
-  ${props => props.transitRight && previousMixin}
-`
-
-const currentLeftMixin = css`
-  transform-origin: 0 50%;
-  animation: ${rotateLeftSideFirstKeyframes} .8s both ease-in;
-`
-const currentRightMixin = css`
-  transform-origin: 0 50%;
-  animation: ${rotateRightSideFirstKeyframes} .8s both ease-in;
-`
-const CurrentMarkdown = styled(Markdown)<{
-  transitRight: boolean
-  transitLeft: boolean
-}>`
-  z-index: 5;
-  left:0;
- 
-  ${props => props.transitRight && currentLeftMixin}
-  ${props => props.transitLeft && currentRightMixin}
-`
-
-const nextMixin = css`
-  animation: ${moveFromRightKeyframes} .6s ease both;
-  animation-delay: .2s;
-`
-const NextMarkdown = styled(Markdown)<{
-  transitLeft: boolean
-}>`
-  z-index: 7;
-  left:100vw;
-  
-  ${props => props.transitLeft && nextMixin}
-`
-
-const Content = styled.div`
-  @media all{
-    font-size: 12px;
-  }
-  @media all and (min-width: 200px) {
-    font-size: 16px;
-  }
-  @media all and (min-width: 400px) {
-    font-size: 20px;
-  }
-  @media all and (min-width: 600px) {
-    font-size: 24px;
-  }
-  @media all and (min-width: 800px) {
-    font-size: 28px;
-  }
-  @media all and (min-width: 1000px) {
-    font-size: 32px;
-  }
-  @media all and (min-width: 1200px){
-    font-size: 36px;
-  }
-  
-  table {
-    width: 100%;
-  }
-`
 
 export const PresentationPage: FC<RouteComponentProps> = (props) => {
   const {slideTexts} = useStore(PresentationStore)
@@ -178,13 +75,8 @@ export const PresentationPage: FC<RouteComponentProps> = (props) => {
 
   const [transit, setTransit] = useState<'previous'| 'next'>(null)
 
-  const previousHtml = useMemo(() => markdownToHtml(slideTexts[currentPage - 1]), [currentPage])
-  const currentHtml = useMemo(() => markdownToHtml(slideTexts[currentPage]), [currentPage])
-  const nextHtml = useMemo(() => markdownToHtml(slideTexts[currentPage + 1]), [currentPage])
-
   const currentSlideRef = useRef<HTMLDivElement>()
   useLayoutEffect(() => {
-    console.log(currentSlideRef.current)
     if (transit === null && currentSlideRef.current) {
       currentSlideRef.current.scrollTop = 0
     }
@@ -251,9 +143,6 @@ export const PresentationPage: FC<RouteComponentProps> = (props) => {
       enterFullscreen()
     }
   }
-  useEffect(()=> {
-    enterFullscreen()
-  }, [])
 
   return (
     <Container>
@@ -262,21 +151,12 @@ export const PresentationPage: FC<RouteComponentProps> = (props) => {
       )}
 
       <Background mouseMoving={mouseMoving}>
-        {previousHtml && (
-          <PreviousMarkdown transitRight={transit === 'previous'} className='markdown'>
-            <Content dangerouslySetInnerHTML={{__html: previousHtml}} className='content'/>
-          </PreviousMarkdown>
-        )}
-        {currentHtml && (
-          <CurrentMarkdown transitRight={transit === 'previous'} transitLeft={transit === 'next'} className='markdown' ref={currentSlideRef}>
-            <Content dangerouslySetInnerHTML={{__html: currentHtml}} className='content'/>
-          </CurrentMarkdown>
-        )}
-        {nextHtml && (
-          <NextMarkdown transitLeft={transit === 'next'} className='markdown'>
-            <Content dangerouslySetInnerHTML={{__html: nextHtml}} className='content'/>
-          </NextMarkdown>
-        )}
+        {Object.keys(SlideMode).map((mode, index) => {
+          const text = slideTexts[currentPage + index - 1]
+          return text && (
+            <Slide transit={transit} markdown={text} mode={mode as SlideMode} key={mode}/>
+          )
+        })}
       </Background>
     </Container>
   )
