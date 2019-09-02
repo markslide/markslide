0dcc0c8524bae4d84e0b2823ca180a47a93187cc
卡晨 <guangbo.hgb@antfin.com>
2019-09-02 10:40:12 +0800

    fix setText and localStorage

--- src/pages/upload.page.tsx
+++ src/pages/upload.page.tsx
@@ -7,11 +7,11 @@ import {PresentationStore} from '@/stores/presentation.store'
 
 export const UploadPage: FC<RouteComponentProps> = (props) => {
   const store = useStore(PresentationStore)
-  
+
   return (
     <div>
       <Upload onUpload={(text) => {
-        store.setText(text)
+        store.updateText(text)
         props.history.push('/presentation')
       }}/>
     </div>
