93377b4f39a7897e5a6a25cc94e781adbb947a9e
卡晨 <guangbo.hgb@antfin.com>
2019-09-02 17:08:09 +0800

    change to hash router

--- src/components/app-router.tsx
+++ src/components/app-router.tsx
@@ -1,7 +1,7 @@
 import {memo, default as React} from 'react'
 import {Route, Switch, Redirect} from 'react-router'
 import {UploadPage} from '@/pages/upload.page'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import {PresentationPage} from '@/pages/presentation.page'
 
 export const AppRouter = memo(function AppRouter() {
