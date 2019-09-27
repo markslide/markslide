044de5750ed83d5d24d1060f0b65d8609b1950e4
awmleer <awmleer@sparker.xyz>
2019-09-27 13:39:47 +0800

    remove highlight lines feature

--- src/components/md-editor.tsx
+++ src/components/md-editor.tsx
@@ -18,14 +18,6 @@ export const SimpleMDEEditor: FC<Props> = (props) => {
   const editorWrapperRef = useRef<HTMLDivElement>()
   const simpleMdeRef = useRef<SimpleMDE>()
   const textareaRef = useRef<HTMLTextAreaElement>()
-  const preElementsRef = useRef<HTMLCollectionOf<HTMLPreElement>>()
-
-  function updateLines() {
-    let lines = document.getElementsByClassName('CodeMirror-code')
-    if (lines.length) {
-      preElementsRef.current = lines[0].getElementsByTagName('pre')
-    }
-  }
 
   useEffect(() => {
     simpleMdeRef.current = new SimpleMDE({
@@ -34,53 +26,24 @@ export const SimpleMDEEditor: FC<Props> = (props) => {
       ...options,
     })
     bindEvents()
-    updateLines()
-    doHighlight()
     console.log(simpleMdeRef.current)
   }, [])
-
-  function doHighlight() {
-    const lines = editPageStore.highlightLines
-
-    //deHighlight all lines
-    const preElements = preElementsRef.current
-    for (let i = 0; i < preElements.length; i++) {
-      const lineEl = preElements[i]
-      lineEl.className = lineEl.className.replace('CodeMirror-line-hl', '')
-    }
-
-    for (let line of lines) {
-      let lineEl = preElements[line]
-      if (lineEl) lineEl.className += ' CodeMirror-line-hl'
-    }
-  }
 
   useEffect(() => {
     let cursor = simpleMdeRef.current.codemirror.getDoc().getCursor()
     simpleMdeRef.current!.codemirror.getDoc().setCursor({
       line: editPageStore.highlightLines[0] || 0,
-      ch: cursor.ch + 1,
+      ch: 0,
     })
   }, [editPageStore.selectedPreview])
 
   useEffect(() => {
-    doHighlight()
     simpleMdeRef.current.codemirror.focus()
   }, [editPageStore.highlightLines])
 
   function bindEvents() {
-    simpleMdeRef.current.codemirror.on("change", updateLines)
     simpleMdeRef.current.codemirror.on("change", () => {
       props.onChange(simpleMdeRef.current.value())
-    })
-    simpleMdeRef.current.codemirror.on("scroll", updateLines)
-    simpleMdeRef.current.codemirror.on("cursorActivity", () => {
-      // TODO: setSelectedPreview 根据cursor的行号更新selectedPreview，保持编辑器和预览器状态一致
-      // let line = simpleMdeRef.current!.codemirror.getDoc().getCursor().line
-      // editPageStore.setSelectedPreview(line)
-    })
-    simpleMdeRef.current.codemirror.on("scroll", () => {
-      doHighlight()
     })
   }
 
