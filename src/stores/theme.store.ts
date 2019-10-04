794f1f90bae9a490b1bb3368ea9d1dff9e7dd3bf
awmleer <awmleer@sparker.xyz>
2019-10-04 20:22:11 +0800

    fix

--- src/stores/theme.store.ts
+++ src/stores/theme.store.ts
@@ -1,8 +1,9 @@
 import {useStorageState} from '@/utils/use-storage-state'
+import {themes} from '@/utils/themes'
 
 export function ThemeStore() {
-  const [theme, setTheme] = useStorageState<string>('theme-id', 'one-dark') // one-dark
-  const [scheme, setScheme] = useStorageState<string>('theme-scheme-id', '')
+  const [theme, setTheme] = useStorageState<string>('theme-id', themes[0].id)
+  const [scheme, setScheme] = useStorageState<string>('theme-scheme-id', themes[0].schemes[0].id)
   
   return {
     theme,
