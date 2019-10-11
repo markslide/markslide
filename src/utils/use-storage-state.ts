5ab033abf9f24d455bdcff1cb75b81dc55e41687
awmleer <awmleer@sparker.xyz>
2019-10-11 20:36:52 +0800

    update useStorageState

--- src/utils/use-storage-state.ts
+++ src/utils/use-storage-state.ts
@@ -4,8 +4,12 @@ export function useStorageState<T extends string>(key: string, defaultValue: T) 
   const [state, setState] = useState<T>(() => (
     localStorage.getItem(key) || defaultValue
   ) as T)
-  function updateState(value: T) {
-    localStorage.setItem(key, value as string)
+  function updateState(value?: T) {
+    if (value === undefined) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, value as string)
+    }
     setState(value)
   }
   return [state, updateState] as [typeof state, typeof updateState]
