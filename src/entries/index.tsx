98b92e5759abd2e127241932f3ad2b12539eaeaf
John_Theo <B_W_Ghost@163.com>
2019-10-16 17:09:18 +0800

    delete theme test

--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom'
 import {App} from '@/components/app'
 import '@/themes/one-dark/index.less'
 import '@/themes/citrine/main.less'
-import '@/themes/test/main.less'
 
 ReactDOM.render(
   <App/>,
